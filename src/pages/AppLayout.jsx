77c8009780057b77e4e6fd0632ed4e05aac05a2f
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-09-09 16:07:15 +0300

    add: pages

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <h1>App Page</h1>
+    </div>
+  );
+}
+
+export default AppLayout;
