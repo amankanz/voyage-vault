a2da3d1783d53ff28d28bd08e861ee8fba9b19e9
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-09-09 17:12:18 +0300

    add: AppLayout component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import Logo from "./Logo.jsx";
+import AppNav from "./AppNav.jsx";
+import Footer from "./Footer.jsx";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
+
